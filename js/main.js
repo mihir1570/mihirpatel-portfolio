5933f707081968a3395623aa343e6aa591ed5be0
Mihir Patel <124758185+mihir1570@users.noreply.github.com>
2024-05-20 13:56:01 +0530

    html css

--- js/main.js
+++ js/main.js
@@ -410,7 +410,7 @@ setupPopup('popupBtn5', 'popupCard5', 'closeBtn5', 'overlay5');
 
 
     document.getElementById("liveIcon").addEventListener("click", function() {
-        window.location.href = "live.html";
+        window.location.href = "liveprojects.html";
     });
 
 
