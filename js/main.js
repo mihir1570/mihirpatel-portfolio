d417a1ce3154c939e4fac77829ca1e0844eff86f
Mihir Patel <124758185+mihir1570@users.noreply.github.com>
2024-06-08 18:15:10 +0530

    add bootstrap cdn 5.0.2 and remove js file in index.html

--- js/main.js
+++ js/main.js
@@ -408,17 +408,16 @@ setupPopup('popupBtn5', 'popupCard5', 'closeBtn5', 'overlay5');
 
 
 
-
-    document.getElementById("liveIcon").addEventListener("click", function() {
-        window.location.href = "liveprojects.html";
-    });
-
+document.getElementById("liveIcon").addEventListener("click", function () {
+    window.location.href = "liveprojects.html";
+});
 
 
 
 
 
-    
+
+
 
 
 
